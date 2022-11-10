9959170df69d15174adc892855f8692c095a9732
Aleksandra-Vainilovich <57612894+Aleksandra-Vainilovich@users.noreply.github.com>
2022-11-10 13:28:51 +0300

    Add files via upload
    pytest

--- TestPyTest.py
+++ TestPyTest.py
@@ -9,7 +9,7 @@ import pytest
 @pytest.fixture(scope='module')
 def conn():
     conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};' #SQLDocker     	DSN=FreeTDS	    SQL Server  ODBC Driver 17 for SQL Server
-                      'Server=EPBYMINW6718;'	#tcp:localhost,1433
+                      'Server=127.0.0.1;'	#tcp:localhost,1433		EPBYMINW6718
                       'Database=TRN_2;'
                       'Trusted_Connection=yes;'
                       'TrustServerCertificate=Yes;')
